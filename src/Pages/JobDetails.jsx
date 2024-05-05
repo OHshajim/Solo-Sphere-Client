0abcbd3c7bc9edcb1a3a54a6e1c68e462bf71f99
OHshajim <ajshajimmax@gmail.com>
2024-05-06 04:15:21 +0600

    bid post

--- src/Pages/JobDetails.jsx
+++ src/Pages/JobDetails.jsx
@@ -1,8 +1,37 @@
 import { useLoaderData } from "react-router-dom"
+import { AuthContext } from "../Provider/AuthProvider"
+import { useState, useContext } from "react";
+import DatePicker from "react-datepicker";
 
+import "react-datepicker/dist/react-datepicker.css";
+import axios from "axios";
 const JobDetails = () => {
   const job = useLoaderData()
-  const {Email, JobTitle, Deadline, Description, Category, MinimumPrice, MaximumPrice} = job
+  const [startDate, setStartDate] = useState(new Date());
+  const { user } = useContext(AuthContext)
+  const { _id, Email, JobTitle, Deadline, Description, Category, MinimumPrice, MaximumPrice } = job;
+  const handleSubmission = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const price = parseFloat(form.price.value);
+    const comment = form.comment.value;
+    const deadline = startDate;
+    const buyer_email = Email;
+    const status = "Pending";
+    const jobId = _id
+    const biding = {
+      jobId, price, email, comment, buyer_email, status, JobTitle, deadline
+    }
+    console.log(biding);
+    try {
+      const { data } = await axios.post(`${import.meta.env.VITE_SERVER_URL}/bids`, biding)
+      console.log(data);
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
   return (
     <div className='flex flex-col md:flex-row justify-around gap-5 my-12 items-center min-h-[calc(100vh-306px)] md:max-w-screen-xl mx-auto '>
       {/* Job Details */}
@@ -49,7 +78,7 @@ const JobDetails = () => {
           Place A Bid
         </h2>
 
-        <form>
+        <form onSubmit={handleSubmission}>
           <div className='grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2'>
             <div>
               <label className='text-gray-700 ' htmlFor='price'>
@@ -71,6 +100,7 @@ const JobDetails = () => {
                 id='emailAddress'
                 type='email'
                 name='email'
+                defaultValue={user?.email}
                 disabled
                 className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md   focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring'
               />
@@ -91,6 +121,7 @@ const JobDetails = () => {
               <label className='text-gray-700'>Deadline</label>
 
               {/* Date Picker Input Field */}
+              <DatePicker className="p-2 w-full rounded-md bg-transparent " selected={startDate} onChange={(date) => setStartDate(date)} />
             </div>
           </div>
 
